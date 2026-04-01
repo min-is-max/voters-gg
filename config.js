c67eb4afc5e7f0e94d77a1aa873ccbbea82583c3
min-is-max <168435062+min-is-max@users.noreply.github.com>
2026-04-01 22:55:14 +0900

    Configure Firebase realtime voting

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.VOTERS_CONFIG = {
-  mode: "local",
+  mode: "firebase",
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyAJzBWSFU6crc9kezVAnhuYiQ9qifEpXps",
+    authDomain: "votergg-27409.firebaseapp.com",
+    databaseURL: "https://votergg-27409-default-rtdb.firebaseio.com",
+    projectId: "votergg-27409",
+    storageBucket: "votergg-27409.firebasestorage.app",
+    messagingSenderId: "910409878252",
+    appId: "1:910409878252:web:530d407f13864b949333c9",
   },
 };
